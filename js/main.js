75ea3be5f2776e296fb31cab87fe141010610bd4
Christian Manriquez <chrmanriquez@gmail.com>
2021-01-03 18:51:06 -0300

    Ingreso archivos para el ejercicio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-
+	var name = $('#name').val();
+	if (name == '') {
+		$ ('#name').parent().append('<span>Campo obligatorio</span>');
+	}
+	var lastname = $('#lastname').val();
+	if (lastname == '') {
+		$ ('#name').parent().append('<span>Campo obligatorio</span>');
+	}
+	
 }
